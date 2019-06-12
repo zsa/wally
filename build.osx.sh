2b1f2364f04f5538c951d6b5d62dee51a2c94dc3
Florian Didron <florian@111studio.jp>
2019-06-12 22:59:05 +0900

    Fix OSX backward compatibility

--- build.osx.sh
+++ build.osx.sh
@@ -3,6 +3,10 @@ cd ui
 yarn bundle
 cd ..
 go-bindata -prefix "ui/build" -o assets.go ui/build/index.dist.js
-go build -gccgoflags "-lusb-1.0" -tags dist -o dist/osx/Wally.app/Contents/MacOS/Wally
-go build -gccgoflags "-lusb-1.0" -o dist/osx/wally-cli cli/main.go
+CGO_LDFLAGS="-mmacosx-version-min=10.8 -lusb-1.0" \
+CGO_CFLAGS=-mmacosx-version-min=10.8 \
+go build -tags dist -o dist/osx/Wally.app/Contents/MacOS/Wally
+CGO_LDFLAGS="-mmacosx-version-min=10.8 -lusb-1.0" \
+CGO_CFLAGS=-mmacosx-version-min=10.8 \
+go build -o dist/osx/wally-cli cli/main.go
 upx dist/osx/wally-cli

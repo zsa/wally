9ab0d20c78aa92c639556a67096887b73e937474
Florian Didron <florian@111studio.jp>
2019-08-05 16:54:44 +0900

    feat: adds os x version build flags to cgo

--- wally/ergoboot.go
+++ wally/ergoboot.go
@@ -3,9 +3,8 @@
 package wally
 
 /*
-
-#cgo darwin CFLAGS: -O2 -Wall
-#cgo darwin LDFLAGS: -framework IOKit -framework CoreFoundation
+#cgo darwin CFLAGS: -O2 -Wall -mmacosx-version-min=10.8
+#cgo darwin LDFLAGS: -framework IOKit -framework CoreFoundation -mmacosx-version-min=10.8
 #include "ergoboot.h"
 
 */

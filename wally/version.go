23d91b6becc5a4c80a10f94d4046dfa80bc565ff
Disco <developer@onkraken.net>
2020-02-26 18:41:13 +0000

    implemented version command for cli

---
+++ wally/version.go
@@ -0,0 +1,17 @@
+package wally
+
+import "runtime"
+
+// GetAppVersion returns the current version number.
+func GetAppVersion() string {
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		return "1.1.2"
+	case "linux":
+		return "1.1.1"
+	case "windows":
+		return "1.1.4"
+	default:
+		return "1.1.0"
+	}
+}

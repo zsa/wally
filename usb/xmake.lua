a75e3b9e05cf50796be258302d0648336f3c0f45
Florian Didron <fd@0x6664.com>
2023-05-09 14:32:28 +0700

    feat: use zsa's usb absraction lib

--- usb/xmake.lua
+++
@@ -1,9 +0,0 @@
-set_allowedarchs("x64", "arm64")
-set_languages("c99", "c++20")
-
-add_requires("libusb", "hidapi")
-
-target("usbtest")
-  set_kind("binary")
-  add_files("./*.cpp")
-  add_packages("libusb", "hidapi")
